75079b47c4e6603ff9a21370f6a3d1e852752722
Tomas Achmedovas <achmedovas.tomas@gmail.com>
2021-11-09 11:37:42 +0200

    Add body & print logs single lined

--- server.js
+++ server.js
@@ -19,7 +19,22 @@ var proxy = httpProxy.createProxyServer({
 }).listen(port);
 
 proxy.on('proxyReq', function(proxyReq, req, res, options) {
-  console.log({msg: 'request successfully proxied', target: req.url, headers: req.headers, body: req.body, statusCode: res.statusCode});
+  var body = '';
+
+  req.on('data', (chunk) => {
+    body += chunk;
+  });
+
+  req.on('end', () => {
+    try {
+      body = JSON.parse(body);
+    } catch (e) {
+      console.warn(JSON.stringify({msg: 'Invalid Body JSON', err: e}));
+    }
+
+    console.log(JSON.stringify({msg: 'request successfully proxied', target: req.url, headers: req.headers, body: body, statusCode: res.statusCode}));
+  });
+
 });
 
-console.log({msg: 'started', 'port': port, target: target});
+console.log(JSON.stringify({msg: 'started', 'port': port, target: target}));
